9d114c82d8e069961c90085ff18cc496354d359b
lucasmargni <lucasmargni@gmail.com>
2026-06-05 18:23:32 -0300

    parent-children relationship

--- nestjs-olympus-api/src/modules/beings/being.entity.ts
+++ nestjs-olympus-api/src/modules/beings/being.entity.ts
@@ -2,6 +2,8 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
+  ManyToMany,
   PrimaryGeneratedColumn,
   TableInheritance,
   UpdateDateColumn,
@@ -24,6 +26,17 @@ export abstract class MythologicalBeing {
 
   @Column({ type: 'varchar', nullable: true })
   romanName!: string | null;
+
+  @ManyToMany(() => MythologicalBeing, (being) => being.children)
+  @JoinTable({
+    name: 'being_relationships',
+    joinColumn: { name: 'child_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'parent_id', referencedColumnName: 'id' },
+  })
+  parents!: MythologicalBeing[];
+
+  @ManyToMany(() => MythologicalBeing, (being) => being.parents)
+  children!: MythologicalBeing[];
 
   @CreateDateColumn()
   createdAt!: Date;
